2246216f5fe5e1ccd37abaf216c3b94116c36139
Sanat-07 <bgenbaevsanat@gmail.com>
2025-10-24 15:55:19 +0500

    Add all 11 devices to menu with full controls

--- src/Main.java
+++ src/Main.java
@@ -30,6 +30,10 @@ public class Main {
         SecurityCamera camera = new SecurityCamera();
         Door door = new Door();
         SmartTv tv = new SmartTv();
+        AirConditioner ac = new AirConditioner();
+        SmartLock lock = new SmartLock();
+        SmartBlinds blinds = new SmartBlinds();
+        RobotVacuum vacuum = new RobotVacuum();
         
         door.turnOn();
 
@@ -42,6 +46,10 @@ public class Main {
         Device RemoteSmartMusic = new RemoteAccessDecorator(new EnergySavingDecorator(musicSystem));
         Device RemoteSmartThermostat = new RemoteAccessDecorator(new EnergySavingDecorator(thermostat));
         Device RemoteSmartDoor = new RemoteAccessDecorator(new EnergySavingDecorator(door));
+        Device RemoteSmartAC = new RemoteAccessDecorator(new EnergySavingDecorator(ac));
+        Device RemoteSmartLock = new RemoteAccessDecorator(new EnergySavingDecorator(lock));
+        Device RemoteSmartBlinds = new RemoteAccessDecorator(new EnergySavingDecorator(blinds));
+        Device RemoteSmartVacuum = new RemoteAccessDecorator(new EnergySavingDecorator(vacuum));
 
         HomeAutomationFacade home = new HomeAutomationFacade(
             (Device)light, (Device)musicSystem, (Device)thermostat, (Device)camera, (Device)door
@@ -58,32 +66,76 @@ public class Main {
             }
 
             if (choice.equals("1")) {
-                System.out.println("Remote Control Options: ");
+                System.out.println("\n=== Remote Control Options ===");
+                System.out.println("Facade Commands:");
                 System.out.println("[1] Start Party Mode");
                 System.out.println("[2] Activate Night Mode");
                 System.out.println("[3] Leave Home");
-                System.out.println("[4] Switch to Voice Control");
-                System.out.println("[5] Light on");
-                System.out.println("[6] Light off");
-                System.out.println("[7] Music on");
-                System.out.println("[8] Music off");
-                System.out.println("[9] set Thermostat");
-                System.out.println("[10] TV");
+                System.out.println("\nDevice Controls:");
+                System.out.println("[4] Light on/off");
+                System.out.println("[5] Music on/off");
+                System.out.println("[6] Thermostat (set temp)");
+                System.out.println("[7] TV on");
+                System.out.println("[8] Air Conditioner");
+                System.out.println("[9] Smart Lock");
+                System.out.println("[10] Smart Blinds");
+                System.out.println("[11] Robot Vacuum");
+                System.out.println("[0] Back to main menu");
+                System.out.print("Choose: ");
                 String opt = sc.nextLine();
 
                 switch (opt) {
                     case "1": home.startPartyMode(); break;
                     case "2": home.activateNightMode(); break;
                     case "3": home.leaveHome(); break;
-                    case "4": choice = "2"; break;
-                    case "5": RemoteSmartLight.turnOn(); break;
-                    case "6": RemoteSmartLight.turnOff(); break;
-                    case "7": RemoteSmartMusic.turnOn(); break;
-                    case "8": RemoteSmartMusic.turnOff(); break;
-                    case "9": System.out.print("Enter temperature (°C): ");
+                    case "4": 
+                        System.out.print("Light [1]On [2]Off: ");
+                        if (sc.nextLine().equals("1")) RemoteSmartLight.turnOn();
+                        else RemoteSmartLight.turnOff();
+                        break;
+                    case "5": 
+                        System.out.print("Music [1]On [2]Off: ");
+                        if (sc.nextLine().equals("1")) RemoteSmartMusic.turnOn();
+                        else RemoteSmartMusic.turnOff();
+                        break;
+                    case "6": 
+                        System.out.print("Enter temperature (°C): ");
                         int t = Integer.parseInt(sc.nextLine());
-                        RemoteSmartThermostat.set(t); break;
-                    case "10": tv.turnOn(); break;
+                        RemoteSmartThermostat.set(t); 
+                        break;
+                    case "7": tv.turnOn(); break;
+                    case "8": 
+                        System.out.println("AC: [1]On [2]Off [3]Set temp [4]Eco mode");
+                        String acOpt = sc.nextLine();
+                        if (acOpt.equals("1")) RemoteSmartAC.turnOn();
+                        else if (acOpt.equals("2")) RemoteSmartAC.turnOff();
+                        else if (acOpt.equals("3")) {
+                            System.out.print("Temperature: ");
+                            RemoteSmartAC.set(Integer.parseInt(sc.nextLine()));
+                        }
+                        else if (acOpt.equals("4")) RemoteSmartAC.ecoMode();
+                        break;
+                    case "9": 
+                        System.out.print("Lock [1]Unlock [2]Lock: ");
+                        if (sc.nextLine().equals("1")) RemoteSmartLock.turnOn();
+                        else RemoteSmartLock.turnOff();
+                        break;
+                    case "10": 
+                        System.out.println("Blinds: [1]Open [2]Close [3]Set level");
+                        String blindOpt = sc.nextLine();
+                        if (blindOpt.equals("1")) RemoteSmartBlinds.turnOn();
+                        else if (blindOpt.equals("2")) RemoteSmartBlinds.turnOff();
+                        else if (blindOpt.equals("3")) {
+                            System.out.print("Level (0-100): ");
+                            RemoteSmartBlinds.set(Integer.parseInt(sc.nextLine()));
+                        }
+                        break;
+                    case "11": 
+                        System.out.print("Vacuum [1]Start [2]Stop: ");
+                        if (sc.nextLine().equals("1")) RemoteSmartVacuum.turnOn();
+                        else RemoteSmartVacuum.turnOff();
+                        break;
+                    case "0": break;
                     default: System.out.println("Invalid option.");
                 }
             }
